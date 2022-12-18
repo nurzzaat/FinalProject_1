0aa0610ffeb5b7a11f6e59dab339522396e6021d
Tasik1 <sbidaishiev@gmail.com>
2022-12-18 18:41:40 +0600

    hotels

--- hotels/certain_hotel_scripts.js
+++ hotels/certain_hotel_scripts.js
@@ -2,21 +2,22 @@
 function switchHearts(){
     const show = document.getElementById("show");
     const hide = document.getElementById("hide");
+    sessionStorage.setItem("isLiked", "Liked");
     if(show.style.display === "none"){
         show.style.display = "block";
         hide.style.display = "none";
-        document.getElementById("switchText").innerHTML = "Сохранено";
+        document.getElementById("switchText").innerHTML = "Liked";
     }else{
         hide.style.display = "block";
         show.style.display = "none";
-        document.getElementById("switchText").innerHTML = "Сохранить";
+        document.getElementById("switchText").innerHTML = "Like";
     }
 }
 
 function hotel(){
     const hotelID = sessionStorage.getItem("hotelID");
     switch (hotelID) {
-        case "kolsai_lakes_town": document.getElementById("hotel_image").src = "./images/kolsay_lakes_town.jpg"; break;
+        case "kolsai_lakes_town": document.getElementById("hotel_image").src = "images/hotels_photo/kolsay_lakes_town/kolsay_lakes_town_in.jpg"; break;
         case "manor_kolsay": document.getElementById("hotel_image").src = "./images/manor_kolsay.jpg"; break;
         case "kolsay_grand": document.getElementById("hotel_image").src = "./images/Kolsay_Grand1.jpg"; break;
         case "kolsay_nomads": document.getElementById("hotel_image").src = "./images/kolsay_nomads.jpg"; break;
