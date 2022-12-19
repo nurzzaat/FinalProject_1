e54df567f83add83027d745f1f2a12c92be0f836
Tasik1 <sbidaishiev@gmail.com>
2022-12-19 11:53:16 +0600

    hotels database

--- hotels/certain_hotel_scripts.js
+++ hotels/certain_hotel_scripts.js
@@ -1,9 +1,8 @@
-
-function switchHearts(){
+function checkLiked() {
+    const isLiked = sessionStorage.getItem("isLiked");
     const show = document.getElementById("show");
     const hide = document.getElementById("hide");
-    sessionStorage.setItem("isLiked", "Liked");
-    if(show.style.display === "none"){
+    if(isLiked === "Liked"){
         show.style.display = "block";
         hide.style.display = "none";
         document.getElementById("switchText").innerHTML = "Liked";
@@ -13,14 +12,19 @@ function switchHearts(){
         document.getElementById("switchText").innerHTML = "Like";
     }
 }
+function switchHearts(){
+    const show = document.getElementById("show");
+    const hide = document.getElementById("hide");
 
-function hotel(){
-    const hotelID = sessionStorage.getItem("hotelID");
-    switch (hotelID) {
-        case "kolsai_lakes_town": document.getElementById("hotel_image").src = "images/hotels_photo/kolsay_lakes_town/kolsay_lakes_town_in.jpg"; break;
-        case "manor_kolsay": document.getElementById("hotel_image").src = "./images/manor_kolsay.jpg"; break;
-        case "kolsay_grand": document.getElementById("hotel_image").src = "./images/Kolsay_Grand1.jpg"; break;
-        case "kolsay_nomads": document.getElementById("hotel_image").src = "./images/kolsay_nomads.jpg"; break;
-        case "tau_kolsay": document.getElementById("hotel_image").src = "./images/tau_kolsay.jpeg"; break;
+    if(show.style.display === "none"){
+        sessionStorage.setItem("isLiked", "Liked");
+        show.style.display = "block";
+        hide.style.display = "none";
+        document.getElementById("switchText").innerHTML = "Liked";
+    }else{
+        sessionStorage.setItem("isLiked", "Like");
+        hide.style.display = "block";
+        show.style.display = "none";
+        document.getElementById("switchText").innerHTML = "Like";
     }
 }
