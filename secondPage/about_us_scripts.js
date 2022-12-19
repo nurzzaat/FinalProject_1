b3948e0deee5f17d1c76c2d6a3158bdd1612ac4c
nurzzaat <210103336@stu.sdu.edu.kz>
2022-12-19 11:55:29 +0600

    new_one

--- secondPage/about_us_scripts.js
+++ secondPage/about_us_scripts.js
@@ -19,31 +19,3 @@ function closeNav() {
     document.getElementsByClassName("section")[0].style.marginLeft = "1%";
     document.getElementsByTagName("header")[0].style.backgroundColor = "white";
 }
-
-
-let slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-}
