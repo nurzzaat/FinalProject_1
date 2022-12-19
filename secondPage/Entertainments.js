0e74046a2f521c0cda8e9e16713eaa952a644b54
nurzzaat <210103336@stu.sdu.edu.kz>
2022-12-19 12:10:46 +0600

    new_one

---
+++ secondPage/Entertainments.js
@@ -0,0 +1,38 @@
+function callHotel(id) {
+    switch (id) {
+        case "id1":
+            sessionStorage.setItem("hotelID", "id1");
+            document.getElementById("id1").href = "./inEntertainments.html";
+            break;
+        case "id2":
+            sessionStorage.setItem("hotelID", "id2");
+            document.getElementById("id2").href = "./inEntertainments.html";
+            break;
+        case "id3":
+            sessionStorage.setItem("hotelID", "id3");
+            document.getElementById("id3").href = "./inEntertainments.html";
+            break;
+        case "id4":
+            sessionStorage.setItem("hotelID", "id4");
+            document.getElementById("id4").href = "./inEntertainments.html";
+            break;
+
+        case "id5":
+            sessionStorage.setItem("hotelID", "id5");
+            document.getElementById("id5").href = "./inEntertainments.html";
+            break;
+        case "id6":
+            sessionStorage.setItem("hotelID", "id6");
+            document.getElementById("id6").href = "./inEntertainments.html";
+            break;
+
+        case "id7":
+            sessionStorage.setItem("hotelID", "id7");
+            document.getElementById("id7").href = "./inEntertainments.html";
+            break;
+        case "id8":
+            sessionStorage.setItem("hotelID", "id8");
+            document.getElementById("id8").href = "./inEntertainments.html";
+            break;
+    }
+}
---
+++ secondPage/inhotels.js
@@ -0,0 +1,77 @@
+
+function hotel(){
+    const hotelID = sessionStorage.getItem("hotelID");
+    switch (hotelID) {
+        case "Guest_House_Kolsai_Lakes": document.getElementById("hotel_image").src = "https://cf.bstatic.com/xdata/images/hotel/max1280x900/273402489.jpg?k=858941fe8c9dc496fb65b37a247def0d4f865a43d50d11ffd5d95a44b746ef59&o=&hp=1";
+            document.getElementById("hotel_description").innerHTML = "Guests home Aisha is situated in Saty, within 7.4 km of Kurmenty and 8.3 km of Ashil-Togay. The property is around 16 km from Karabulak, 19 km from Imeni Stalina and 23 km from Kolʼbastau. The guest house has family rooms.All rooms in the guest house are fitted with a kettle. At гостевой дом Aisha the rooms come with bed linen and towels.Guests at the accommodation can enjoy a à la carte breakfast.Zhalangash is 26 km from гостевой дом Aisha, while Kolʼbastau is 27 km from the property. The nearest airport is Almaty International Airport, 258 km from the guest house. Couples particularly like the location — they rated it 8.9 for a two-person trip.";
+            document.getElementById("hotel_name").innerHTML = "@Guest_House_Kolsai_Lakes";
+            document.getElementById("hotel_name").href = "#";
+            document.getElementById("hotel_location").innerHTML = " Street S. Musabekuly h.12, 041422 Saty, Kazakhstan";
+            document.getElementById("hotel_room").src = "https://cdn.shymbulak-dev.com/media/c511443542926e5f20997bbcee87cdc4.png";
+            document.getElementById("hotel_room_description").innerHTML = "Double room with one bed. Accommodation suitable for 2 adults and 1 child.";
+            document.getElementById("Title").innerHTML = "Standard";
+            document.getElementById("hotel_rooms_area").innerHTML = "Area: 18 m²";
+            document.getElementById("hotel_rooms_person").innerHTML = "Person: 4";
+            document.getElementById("hotel_room_price").innerHTML = "from 30000 tg";
+            document.getElementById("hotels_name").innerHTML = "/ Guest House Kolsai Lakes";
+            document.getElementById("hotels_names_head").innerHTML = "Guest House Kolsai Lakes";
+            break;
+        case "Bereke_Kolsay_Resort": document.getElementById("hotel_image").src = "https://cdn.shymbulak-dev.com/media/7e3ce17e1caf0f49bda7e25b11a0415d.jpg";
+            document.getElementById("hotel_description").innerHTML = "Offering mountain views, Bereke Kolsay Resort is an accommodation situated in Saty, 16 km from Karabulak and 19 km from Imeni Stalina. With free private parking, the property is 6.7 km from Kurmenty and 7.6 km from Ashil-Togay. The holiday home consists of 1 bedroom, a living room, a fully equipped kitchenette with a microwave and a kettle, and 1 bathroom with a shower and a hairdryer. Towels and bed linen are available in this accommodation. American and Asian breakfast options are available each morning at the holiday home. Kolʼbastau is 23 km from Bereke Kolsay Resort, while Zhalangash is 27 km from the property. The nearest airport is Almaty International Airport, 258 km from the accommodation.";
+            document.getElementById("hotel_name").innerHTML = "@Bereke_Kolsay_Resort";
+            document.getElementById("hotel_name").href = "#";
+            document.getElementById("hotel_location").innerHTML = "Ultarakova 60A, Saty, Kazakhstan";
+            document.getElementById("hotel_room").src = "https://cf.bstatic.com/xdata/images/hotel/max1024x768/373838924.jpg?k=4b8ed977d282243ca7f8d3810ff9c2374ea4defc1e8f8553701ea8a968673f0a&o=&hp=1";
+            document.getElementById("hotel_room_description").innerHTML = "Double room with one bed. Accommodation suitable for 2 adults and 1 child.";
+            document.getElementById("Title").innerHTML = "LUX";
+            document.getElementById("hotel_rooms_area").innerHTML = "Area: 27 m²";
+            document.getElementById("hotel_rooms_person").innerHTML = "Person: 3";
+            document.getElementById("hotel_room_price").innerHTML = "from 50000 tg";
+            document.getElementById("hotels_name").innerHTML = "/ Bereke Kolsay Resort";
+            document.getElementById("hotels_names_head").innerHTML = "Bereke Kolsay Resort";
+            break;
+
+        case "Kolsay_Lakes_Town": document.getElementById("hotel_image").src = "https://cf.bstatic.com/xdata/images/hotel/max1280x900/265092345.jpg?k=cdc31ab1e538a15aea71c52de00e7ebed716beefe1e6458dcc471b0fc9890643&o=&hp=1";
+            document.getElementById("hotel_description").innerHTML = " Kolsay Lakes Town on the shore of the lower lake Kolsay. We are located in the Kolsay Kolderi National Park, 300 km from Almaty";
+            document.getElementById("hotel_name").innerHTML = "@Kolsay_Lakes_Town";
+            document.getElementById("hotel_name").href = "https://www.instagram.com/kolsay_lakes_town/";
+            document.getElementById("hotel_location").innerHTML = "Ultarakova street, house 1a, building 1 sati.";
+            document.getElementById("hotel_room").src = "https://cdn.shymbulak-dev.com/media/366bd329ff884ef1a43ebfb522aa7b24.png";
+            document.getElementById("hotel_room_description").innerHTML = "Double room with one bed. Accommodation suitable for 2 adults and 1 child.";
+            document.getElementById("Title").innerHTML = "Hostel for 4 people";
+            document.getElementById("hotel_rooms_area").innerHTML = "Area: 32 m²";
+            document.getElementById("hotel_rooms_person").innerHTML = "Person: 4";
+            document.getElementById("hotel_room_price").innerHTML = "from 47000 tg";
+            document.getElementById("hotels_name").innerHTML = "/ Kolsay Lakes Town";
+            document.getElementById("hotels_names_head").innerHTML = "Kolsay Lakes Town";
+            break;
+        case "Kolsay_Lakes_Nomads": document.getElementById("hotel_image").src = "https://cdn.shymbulak-dev.com/media/7b883d193922fd4eacece42c49cf6683.jpg";
+            document.getElementById("hotel_description").innerHTML = "Kolsai Nomads is a boutique style Eco-Friendly Resort in Kolsai Region. It is located on the 1st lake, which offers a magnificent view of the mirror water surface. At your service 2 and 4 seater Eco-House and Glamping, equipped with everything necessary for your comfort and relaxation.";
+            document.getElementById("hotel_name").innerHTML = "@Kolsay_Lakes_Nomads";
+            document.getElementById("hotel_name").href = "https://www.instagram.com/kolsay.nomads/?hl=en";
+            document.getElementById("hotel_location").innerHTML = "It is located on the 1st lake, which offers a magnificent view of the mirror water surface.";
+            document.getElementById("hotel_room").src = "https://cdn.shymbulak-dev.com/media/6d07b19b4ff42f2ebd74a56b7d5efe29.jpg";
+            document.getElementById("hotel_room_description").innerHTML = "A cozy chalet, for 8 persons, with 5 bathrooms, a dining table for 8 persons, a fully equipped kitchen, a swimming pool with sauna, maid service.";
+            document.getElementById("Title").innerHTML = "Chalet";
+            document.getElementById("hotel_rooms_area").innerHTML = "Area: 30 m²";
+            document.getElementById("hotel_rooms_person").innerHTML = "Person: 2";
+            document.getElementById("hotel_room_price").innerHTML = "from 80000 tg";
+            document.getElementById("hotels_name").innerHTML = "/  Kolsay Lakes Nomads";
+            document.getElementById("hotels_names_head").innerHTML = "Kolsay Lakes Nomads";
+            break;
+        case "Glamping_Kolsay":     document.getElementById("hotel_image").src = "https://www.economistjurist.es/wp-content/uploads/sites/2/2022/08/Foto-1.jpg";
+            document.getElementById("hotel_description").innerHTML = "Glamping (glamorous camping) - recreation with a high level of comfort. Glamping is gaining popularity around the world, including Kazakhstan. This format of recreation is designed specifically for those who want to relax in nature without sacrificing the usual amenities. For example, instead of canned food and beach packages, tourists are offered à la carte dishes, and instead of a tent, a tent with conditions for a good rest. There are several types of glamping: tent, trailer, tent and dome. The material collected glampings in Almaty and Almaty region, which will be of interest to lovers of luxury outdoor recreation.";
+            document.getElementById("hotel_name").innerHTML = "@Glamping_Kolsay";
+            document.getElementById("hotel_name").href = "https://www.instagram.com/heaven.glamping.almaty/?hl=en";
+            document.getElementById("hotel_location").innerHTML = "Uzynagash village, st. Almalybak, 100";
+            document.getElementById("hotel_room").src = "https://lh5.googleusercontent.com/sX1omVgqaoFkLAVXj3fnKzn_23I2IyJqsCC90OP63u84213Mwj5cb1NZKLJT8GmAdEcu-SZN3NCr6_M_s5jTjeK3xyX5rwJqSSxKSjSdf7CyJuid_-2gVCkj12sOCXcBf8VRx6_2";
+            document.getElementById("hotel_room_description").innerHTML = "Here you can arrange a small corporate party, a romantic evening or a family holiday for up to 15 people. The cost of glamping includes a wood-fired sauna, a grill house and a vat.";
+            document.getElementById("Title").innerHTML = "Standart";
+            document.getElementById("hotel_rooms_area").innerHTML = "Area: 28 m²";
+            document.getElementById("hotel_rooms_person").innerHTML = "Person: 4";
+            document.getElementById("hotel_room_price").innerHTML = "from 30000 tg";
+            document.getElementById("hotels_name").innerHTML = "/  Glamping Kolsay";
+            document.getElementById("hotels_names_head").innerHTML = "Glamping Kolsay";
+            break;
+    }
+}
